fb56f50d2b46dfca5c524da376dfa91bc209a67b
a.shinyakov <ashinyakov@bk.ru>
2025-02-17 00:24:13 +0300

    Add Security

--- ApiService/build.gradle.kts
+++ ApiService/build.gradle.kts
@@ -27,7 +27,7 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	//implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.3.0")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
--- ApiService/src/main/kotlin/ru/itmo/ai/school/ecom/api/apiservice/configuration/SecurityConfiguration.kt
+++ ApiService/src/main/kotlin/ru/itmo/ai/school/ecom/api/apiservice/configuration/SecurityConfiguration.kt
@@ -1,27 +1,43 @@
 package ru.itmo.ai.school.ecom.api.apiservice.configuration
 
-//import org.springframework.context.annotation.Bean
-//import org.springframework.context.annotation.Configuration
-//import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
-//import org.springframework.security.config.web.server.SecurityWebFiltersOrder
-//import org.springframework.security.config.web.server.ServerHttpSecurity
-//import org.springframework.security.web.server.SecurityWebFilterChain
-//import ru.itmo.ai.school.ecom.api.apiservice.security.JwtAuthenticationWebFilter
-//
-//@Configuration
-//@EnableWebFluxSecurity
-//class SecurityConfiguration(private val jwtAuthenticationWebFilter: JwtAuthenticationWebFilter) {
-//
-//    @Bean
-//    fun securityWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
-//        return http
-//            .csrf { it.disable() }
-//            .authorizeExchange { exchanges ->
-//                exchanges.anyExchange().authenticated()
-//            }
-//            .addFilterAfter(jwtAuthenticationWebFilter, SecurityWebFiltersOrder.AUTHENTICATION)
-//            .build()
-//    }
-//}
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.security.web.server.authentication.AuthenticationWebFilter
+import org.springframework.security.web.server.authentication.ServerAuthenticationConverter
+import org.springframework.security.authentication.ReactiveAuthenticationManager
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder
+import ru.itmo.ai.school.ecom.api.apiservice.security.JwtReactiveAuthenticationManager
+import ru.itmo.ai.school.ecom.api.apiservice.security.JwtServerAuthenticationConverter
+
+@Configuration
+class SecurityConfiguration {
+
+    @Bean
+    fun securityWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
+        return http
+            .csrf { it.disable() }
+            .authorizeExchange { exchanges ->
+                exchanges
+                    .anyExchange().authenticated()
+            }
+            .authenticationManager(jwtReactiveAuthenticationManager())
+            .addFilterAt(authenticationWebFilter(), SecurityWebFiltersOrder.AUTHENTICATION)
+            .build()
+    }
+
+    @Bean
+    fun jwtReactiveAuthenticationManager(): ReactiveAuthenticationManager {
+        return JwtReactiveAuthenticationManager()
+    }
+
+    @Bean
+    fun authenticationWebFilter(): AuthenticationWebFilter {
+        val authFilter = AuthenticationWebFilter(jwtReactiveAuthenticationManager())
+        authFilter.setServerAuthenticationConverter(JwtServerAuthenticationConverter())
+        return authFilter
+    }
+}
 
 
